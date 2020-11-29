fb84a75508e19fffe240bc1a6f87f8dfdc20e956
theoye <theoye888@gmai.com>
2020-11-29 14:44:20 +0800

    Feature: add CMakeLists

---
+++ TCPclient.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+int main(){
+	//创建套接字
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	//服务器的ip为本地，端口号1234
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_port = htons(1234);
+	
+	//向服务器发送连接请求
+	connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	//发送并接收数据
+	char buffer[40];
+	printf("Please write:");
+	scanf("%s", buffer);
+	while(strcmp(buffer, "end")!=0){
+		memset(&buffer, 0, sizeof(buffer));
+		//循环给服务器发送a-z
+		for(int i= 0 ; i < 26;i++){
+			buffer[0] = 'a'+i;
+			write(sock, buffer, sizeof(buffer));
+			sleep(0.5);  //睡眠0.5s
+		}
+
+		printf("Please write:");
+		scanf("%s", buffer);
+	}
+	strcpy(buffer, "over");
+	write(sock, buffer, sizeof(buffer));
+	read(sock, buffer, sizeof(buffer) - 1);
+	printf("Server send: %s\n", buffer);
+
+	//断开连接
+	close(sock);
+
+	return 0;
+}
